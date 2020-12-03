b3e09b6f45ed0a91bff30520a317631616c95032
amankmanish <amanroy281205@gmail.com>
2020-12-03 18:04:35 +0530

    creating Components for user

--- e-shop-application/src/app/app.module.ts
+++ e-shop-application/src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { StoreComponent } from './components/store/store.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserOrdersComponent } from './components/user-orders/user-orders.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    CartComponent,
+    LoginComponent,
+    StoreComponent,
+    ProductCardComponent,
+    SignupComponent,
+    UserOrdersComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
