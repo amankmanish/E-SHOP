39419f539d7fb4050b24a321e0ff07d467b68aa5
amankmanish <amanroy281205@gmail.com>
2020-12-03 18:40:01 +0530

    Adding Navigation

--- e-shop-application/src/app/app-routing.module.ts
+++ e-shop-application/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserOrdersComponent } from './components/user-orders/user-orders.component';
 
 
-const routes: Routes = [{
-  path :'home' , component : HomeComponent
-}];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {path :'home' , component : HomeComponent},
+  { path: 'orders', component: UserOrdersComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- e-shop-application/src/app/app.module.ts
+++ e-shop-application/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CartComponent } from './components/cart/cart.component';
@@ -13,6 +12,7 @@ import { ProductCardComponent } from './components/product-card/product-card.com
 import { SignupComponent } from './components/signup/signup.component';
 import { UserOrdersComponent } from './components/user-orders/user-orders.component';
 import { FilterComponent } from './components/filter/filter.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
